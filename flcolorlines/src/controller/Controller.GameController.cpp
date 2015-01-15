f63236f894f6076b2dc5def19bb55487fbf08f50
fury lynx <furylynx@users.noreply.github.com>
2015-01-15 15:34:04 +0100

    documentation,refactoring -> continued.

--- flcolorlines/src/controller/Controller.GameController.cpp
+++ flcolorlines/src/controller/Controller.GameController.cpp
@@ -2,41 +2,48 @@
 
 using namespace controller;
 
-GameController::GameController(){
-    theField = 0;
+GameController::GameController()
+{
+    theField = nullptr;
     actionController = new controller::GameActionController();
 }
 
-GameController::~GameController(){
+GameController::~GameController()
+{
     delete theField;
     delete actionController;
 }
 
+void GameController::setOptions(int dimx, int dimy, int amountColors, int amountNewBalls, int amountLine)
+{
+    actionController->setOptions(nullptr);
 
-void GameController::setOptions(int dimx, int dimy, int amount_colors, int amount_newballs, int amount_line){
-    actionController->setOptions(0);
-
-    if (theField != 0)
+    if (theField != nullptr)
+    {
         delete theField;
+    }
 
     theField = new model::GameField(dimx, dimy);
     actionController->setOptions(theField);
 
 
-    this->amount_colors = amount_colors;
-    this->amount_newballs = amount_newballs;
-    this->amount_line = amount_line;
+    this->amountColors = amountColors;
+    this->amountNewBalls = amountNewBalls;
+    this->amountLine = amountLine;
 
     optionsSet = true;
 }
 
-bool GameController::areOptionsSet(){
+bool GameController::areOptionsSet()
+{
     return optionsSet;
 }
 
-void GameController::startNewMatch(){
+void GameController::startNewMatch()
+{
 
-    if (!optionsSet){
+    if (!optionsSet)
+    {
         std::cerr << "No options defined, default used.";
 
         actionController->setOptions(0);
@@ -47,68 +54,62 @@ void GameController::startNewMatch(){
 
     theScore = 0;
     theField->clear();
-    if(actionController != 0)
+    if(actionController != nullptr)
+    {
         actionController->clear();
+    }
 
-
-//    //TODO REMOVE!!!
-//    model::FieldPosition pos;
-//    pos.posX = 0;
-//    pos.posY = 1;
-//    model::Ball ball;
-//    ball.color.setColor(model::Color::BLUE);
-//    theField->setBallAt(pos,ball);
-//    pos.posX = 1;
-//    theField->setBallAt(pos,ball);
-//    pos.posX = 2;
-//    theField->setBallAt(pos,ball);
-//    pos.posX = 3;
-//    theField->setBallAt(pos,ball);
-
-//    pos.posX = 5;
-//    theField->setBallAt(pos,ball);
-//    //---
-
-    generateRandomBalls(amount_newballs);
+    generateRandomBalls(amountNewBalls);
 
 }
 
-void GameController::decreaseScore(int c){
+void GameController::decreaseScore(int c)
+{
     theScore -= c;
 }
 
-void GameController::increaseScore(int c){
+void GameController::increaseScore(int c)
+{
     increaseScore(c,true);
 }
 
-void GameController::increaseScore(int c, bool logAction){
+void GameController::increaseScore(int c, bool logAction)
+{
 
     model::GameAction action;
     action.type = model::GameAction::INCREMENTSCORE;
     action.value = (unsigned int) c;
 
-    if (actionController != 0 && logAction)
+    if (actionController != nullptr && logAction)
+    {
         actionController->addAction(action);
+    }
 
     theScore += c;
 }
 
-std::vector<model::FieldPosition> GameController::getPath(model::FieldPosition pos1, model::FieldPosition pos2) const{
+std::vector<model::FieldPosition> GameController::getPath(model::FieldPosition pos1, model::FieldPosition pos2) const
+{
 
     std::vector<model::FieldPosition> ret = theField->getPath(pos1,pos2);
 
     return ret;
 }
 
-bool GameController::moveBallForce(model::FieldPosition pos1, model::FieldPosition pos2){
+bool GameController::moveBallForce(model::FieldPosition pos1, model::FieldPosition pos2)
+{
 
     if (!theField->getBallAt(pos2).isNull || theField->getBallAt(pos2).outOfField)
+    {
         return false;
+    }
 
     model::GameAction action3;
     action3.type = model::GameAction::NEWTURN;
-    if (actionController != 0)
+    if (actionController != nullptr)
+    {
         actionController->addAction(action3);
+    }
 
     //switch ball to position
     model::Ball ball = theField->getBallAt(pos1);
@@ -120,21 +121,29 @@ bool GameController::moveBallForce(model::FieldPosition pos1, model::FieldPositi
     action.type = model::GameAction::ADDBALL;
     action.pos = pos2;
     action.value = ball.color.color;
-    if (actionController != 0)
+    if (actionController != nullptr)
+    {
         actionController->addAction(action);
+    }
 
     model::GameAction action2;
     action2.type = model::GameAction::REMOVEBALL;
     action2.value = ball.color.color;
     action2.pos = pos1;
-    if (actionController != 0)
+    if (actionController != nullptr)
+    {
         actionController->addAction(action2);
+    }
 
     lastRemovedBalls.clear();
     if (!checkLines(pos2))
-        generateRandomBalls(amount_newballs);
+    {
+        generateRandomBalls(amountNewBalls);
+    }
     else
+    {
         lastGeneratedBalls.clear();
+    }
 
     return true;
 
@@ -151,38 +160,40 @@ bool GameController::checkLines(model::FieldPosition pos){
         counters.push_back(0);
 
         if (i == model::GameField::Middle)
-            ;//do nothing cause this is no neighbour
-        else{
-            if (!neighbours[i].isNull && !neighbours[i].outOfField && neighbours[i].color == neighbours[model::GameField::Middle].color){
+        {
+            //do nothing cause this is no neighbour
+        }
+        else
+        {
+            if (!neighbours[i].isNull && !neighbours[i].outOfField && neighbours[i].color == neighbours[model::GameField::Middle].color)
+            {
 
                 model::FieldPosition npos = model::GameField::getPositionOfNeighbour(pos, static_cast<model::GameField::Neighbour>(i));
-                counters[i]++;
+                counters.at(i)++;
 
-                while (!theField->getNeighbourAt(npos,static_cast<model::GameField::Neighbour>(i)).isNull && !theField->getNeighbourAt(npos,static_cast<model::GameField::Neighbour>(i)).outOfField && theField->getNeighbourAt(npos,static_cast<model::GameField::Neighbour>(i)).color == theField->getNeighbourAt(pos,static_cast<model::GameField::Neighbour>(i)).color){
+                while (!theField->getNeighbourAt(npos,static_cast<model::GameField::Neighbour>(i)).isNull && !theField->getNeighbourAt(npos,static_cast<model::GameField::Neighbour>(i)).outOfField && theField->getNeighbourAt(npos,static_cast<model::GameField::Neighbour>(i)).color == theField->getNeighbourAt(pos,static_cast<model::GameField::Neighbour>(i)).color)
+                {
                     counters[i]++;
-
                     npos = model::GameField::getPositionOfNeighbour(npos, static_cast<model::GameField::Neighbour>(i));
-
                 }
             }
-
-
         }
     }
-
-//    std::cout << counters[0] << ", " << counters[1] << ", " << counters[2] << ", " << counters[3] << ", " << counters[5] << ", " << counters[6] << ", " << counters[7] << ", " << counters[8] <<"\n";
 
     //check for lines longer than the set amount
     bool anyLineExistent = false;
 
-    for(int i=model::GameField::Topleft; i <= (int)(model::GameField::BottomRight/2); i++){
+    for(int i=model::GameField::Topleft; i <= (int)(model::GameField::BottomRight/2); i++)
+    {
 
-        if (counters[i]+1+counters[model::GameField::BottomRight-i] >= amount_line){
+        if (counters[i]+1+counters[model::GameField::BottomRight-i] >= amountLine)
+        {
             anyLineExistent = true;
 
             //remove balls
             model::FieldPosition npos = model::GameField::getPositionOfNeighbour(pos, static_cast<model::GameField::Neighbour>(i));
-            for (int j = 1; j<= counters[i];j++){
+            for (int j = 1; j<= counters[i];j++)
+            {
                 model::Ball b = theField->removeBallAt(npos);
                 lastRemovedBalls.push_back(npos);
 
@@ -190,14 +201,17 @@ bool GameController::checkLines(model::FieldPosition pos){
                 action.type = model::GameAction::REMOVEBALL;
                 action.pos = npos;
                 action.value = b.color.color;
-                if (actionController != 0)
+                if (actionController != nullptr)
+                {
                     actionController->addAction(action);
+                }
 
                 npos = model::GameField::getPositionOfNeighbour(npos, static_cast<model::GameField::Neighbour>(i));
             }
 
             npos = model::GameField::getPositionOfNeighbour(pos, static_cast<model::GameField::Neighbour>(model::GameField::BottomRight-i));
-            for (int j = 1; j<= counters[model::GameField::BottomRight-i];j++){
+            for (int j = 1; j<= counters[model::GameField::BottomRight-i];j++)
+            {
                 model::Ball b = theField->removeBallAt(npos);
                 lastRemovedBalls.push_back(npos);
 
@@ -205,15 +219,18 @@ bool GameController::checkLines(model::FieldPosition pos){
                 action.type = model::GameAction::REMOVEBALL;
                 action.pos = npos;
                 action.value = b.color.color;
-                if (actionController != 0)
+                if (actionController != nullptr)
+                {
                     actionController->addAction(action);
+                }
 
                 npos = model::GameField::getPositionOfNeighbour(npos, static_cast<model::GameField::Neighbour>(model::GameField::BottomRight-i));
             }
         }
     }
 
-    if (anyLineExistent){
+    if (anyLineExistent)
+    {
         model::Ball b = theField->removeBallAt(pos);
         lastRemovedBalls.push_back(pos);
 
@@ -221,37 +238,45 @@ bool GameController::checkLines(model::FieldPosition pos){
         action.type = model::GameAction::REMOVEBALL;
         action.pos = pos;
         action.value = b.color.color;
-        if (actionController != 0)
+        if (actionController != nullptr)
+        {
             actionController->addAction(action);
+        }
     }
     else
+    {
         return false;
+    }
 
     //count scores
     int scoreinc = 1;
 
     for(int i=model::GameField::Topleft; i <= (int)(model::GameField::BottomRight/2); i++)
-        scoreinc += counters[i]+counters[model::GameField::BottomRight-i];
+    {
+        scoreinc += counters.at(i) + counters.at(model::GameField::BottomRight-i);
+    }
 
-    increaseScore(scoreinc*scoreinc);//increase score by amount of removed balls squared
+    //increase score by amount of removed balls squared
+    increaseScore(scoreinc*scoreinc);
 
     return true;
 }
 
-std::vector<model::FieldPosition> GameController::moveBall(model::FieldPosition pos1, model::FieldPosition pos2){
+std::vector<model::FieldPosition> GameController::moveBall(model::FieldPosition pos1, model::FieldPosition pos2)
+{
 
     std::vector<model::FieldPosition> path = getPath(pos1, pos2);
 
-    if (path[path.size()-1].posX == pos2.posX && path[path.size()-1].posY == pos2.posY){
+    if (path[path.size()-1].posX == pos2.posX && path[path.size()-1].posY == pos2.posY)
+    {
            moveBallForce(pos1,pos2);
     }
-
 
     return path;
 }
 
-bool GameController::generateRandomBalls(int n){
-
+bool GameController::generateRandomBalls(int n)
+{
 
     lastGeneratedBalls.clear();
     std::vector<model::FieldPosition> freePos = theField->getEmptyFields();
@@ -273,9 +298,9 @@ bool GameController::generateRandomBalls(int n){
         pos = freePos[el];
 
         //random color
-        int col = rand() % amount_colors;
+        int col = rand() % amountColors;
 
-        if (amount_colors > model::Color::AMOUNT_DEFINED_COLORS)
+        if (amountColors > model::Color::AMOUNT_DEFINED_COLORS)
             assert(false);//TODO do something to catch this
 
         //set color for the ball
@@ -295,8 +320,10 @@ bool GameController::generateRandomBalls(int n){
         action.pos = pos;
         action.value = b.color.color;
 
-        if (actionController != 0)
+        if (actionController != nullptr)
+        {
             actionController->addAction(action);
+        }
 
         checkLines(pos);
         freePos = theField->getEmptyFields();
@@ -315,70 +342,97 @@ bool GameController::generateRandomBalls(int n){
     return false;
 }
 
-bool GameController::undoLastTurn(){
-    if (actionController != 0){
+bool GameController::undoLastTurn()
+{
+    if (actionController != nullptr)
+    {
         decreaseScore(actionController->undoLastTurn());
         return actionController->undoPossible();
     }
     else
+    {
         return false;
+    }
 }
 
-bool GameController::redoLastTurn(){
-    if (actionController != 0 && ! isGameOver()){
+bool GameController::redoLastTurn()
+{
+    if (actionController != nullptr && ! isGameOver())
+    {
         increaseScore(actionController->redoLastTurn(), false);
         return actionController->redoPossible();
     }
     else
+    {
         return false;
+    }
 }
 
-bool GameController::undoPossible(){
-    if (actionController != 0 && ! isGameOver()){
+bool GameController::undoPossible()
+{
+    if (actionController != nullptr && ! isGameOver())
+    {
         return actionController->undoPossible();
-    }else
+    }
+    else
+    {
         return false;
+    }
 }
 
-bool GameController::redoPossible(){
-    if (actionController != 0){
+bool GameController::redoPossible()
+{
+    if (actionController != nullptr)
+    {
         return actionController->redoPossible();
-    }else
+    }
+    else
+    {
         return false;
+    }
 }
 
-model::GameField* GameController::getGameField() const{
+model::GameField* GameController::getGameField() const
+{
     return theField;
 }
 
-int GameController::getTheScore() const{
+int GameController::getTheScore() const
+{
     return theScore;
 }
 
-bool GameController::isGameOver() const{
-    if (theField == 0)
+bool GameController::isGameOver() const
+{
+    if (theField == nullptr)
+    {
         return true;
+    }
 
     return theField->getEmptyFields().size() == 0;
 }
 
-int GameController::getAmountColors() const{
-    return amount_colors;
+int GameController::getAmountColors() const
+{
+    return amountColors;
 }
 
-int GameController::getAmountNewBalls() const{
-    return amount_newballs;
+int GameController::getAmountNewBalls() const
+{
+    return amountNewBalls;
 }
 
-int GameController::getAmountLine() const{
-    return amount_line;
+int GameController::getAmountLine() const
+{
+    return amountLine;
 }
 
-
-std::vector<model::FieldPosition> GameController::getLastGeneratedBalls(){
+std::vector<model::FieldPosition> GameController::getLastGeneratedBalls()
+{
     return lastGeneratedBalls;
 }
 
-std::vector<model::FieldPosition> GameController::getLastRemovedBalls(){
+std::vector<model::FieldPosition> GameController::getLastRemovedBalls()
+{
     return lastRemovedBalls;
 }
--- flcolorlines/src/controller/Controller.GameController.hpp
+++ flcolorlines/src/controller/Controller.GameController.hpp
@@ -18,59 +18,235 @@ class GameController
 
 public:
 
+    /**
+     * Creates a new game controller.
+     */
     GameController();
-    ~GameController();
+    virtual ~GameController();
 
-    void setOptions(int dimx, int dimy, int amount_colors, int amount_newballs, int amount_line);
-    bool areOptionsSet();
+    /**
+     * Sets the options for the current game.
+     *
+     * @param dimx Field dimension on the x-axis.
+     * @param dimy Field dimension on the y-axis.
+     * @param amount_colors The number of colors used for the balls.
+     * @param amount_newballs The number of additional balls dropping on a move.
+     * @param amount_line The number of same colored balls in a line required to remove it.
+     */
+    virtual void setOptions(int dimx, int dimy, int amountColors, int amountNewBalls, int amountLine);
 
-    void startNewMatch();//counter to zero, new game field
+    /**
+     * @brief areOptionsSet
+     * @return
+     */
+    virtual bool areOptionsSet();
 
-    std::vector<model::FieldPosition> getPath(model::FieldPosition pos1, model::FieldPosition pos2) const;//if vector doesn't contain pos2 there is no path
-    std::vector<model::FieldPosition> moveBall(model::FieldPosition pos1, model::FieldPosition pos2); // if vector does't contain pos2 the ball was not moved
+    /**
+     * Starts a new game. Sets the counter to zero and sets up a new game field.
+     */
+    virtual void startNewMatch();
 
-    bool moveBallForce(model::FieldPosition pos1, model::FieldPosition pos2);//just moves a ball without taking account of possible ways
+    /**
+     * Returns the shortest possible path between the two field positions.
+     *
+     * @param pos1 The first position.
+     * @param pos2 The second position.
+     * @return The path.
+     */
+    virtual std::vector<model::FieldPosition> getPath(model::FieldPosition pos1, model::FieldPosition pos2) const;//if vector doesn't contain pos2 there is no path
 
-    bool undoLastTurn(); // rearranges the first three balls if first turn ??
-    bool redoLastTurn();
+    /**
+     * Moves the ball from the first to the second position.
+     *
+     * @param pos1 The first position.
+     * @param pos2 The second position.
+     * @return The path. If the vector does not contain the second position, the ball was not moved.
+     */
+    virtual std::vector<model::FieldPosition> moveBall(model::FieldPosition pos1, model::FieldPosition pos2);
 
-    bool undoPossible();
-    bool redoPossible();
+    /**
+     * Forces to move the ball from the first position to the second position even if no path is available.
+     *
+     * @param pos1 The first position.
+     * @param pos2 The second position.
+     * @return True if the movement was possible in terms of having a ball moving a an empty field position.
+     */
+    virtual bool moveBallForce(model::FieldPosition pos1, model::FieldPosition pos2);
 
+    /**
+     * Undoes the last turn.
+     *
+     * @return True if further undo is possible.
+     */
+    virtual bool undoLastTurn();
+
+    /**
+     * Redoes the last undone turn.
+     *
+     * @return True if further redo is possible.
+     */
+    virtual bool redoLastTurn();
+
+    /**
+     * Check whether further undo is possible.
+     *
+     * @return True if further undo is possible.
+     */
+    virtual bool undoPossible();
+
+    /**
+     * Check whether further redo is possible.
+     *
+     * @return True if further redo is possible.
+     */
+    virtual bool redoPossible();
+
+    /**
+     * Increase the score by the given value.
+     *
+     * @param c The amount to increase.
+     */
     void increaseScore(int c);
+
+    /**
+     * Increase the score by the given value. Additionally, the action is logged if the value is set.
+     *
+     * @param c The amount to increase.
+     * @param logAction True if the action is intended to be logged.
+     */
     void increaseScore(int c, bool logAction);
+
+    /**
+     * Decreases the score by the given value.
+     *
+     * @param c The amount to decrease.
+     */
     void decreaseScore(int c);
 
-    model::GameField* getGameField() const;
-    int getTheScore() const;
-    int getAmountColors() const;
-    int getAmountNewBalls() const;
-    int getAmountLine() const;
+    /**
+     * Returns the game field.
+     *
+     * @return The game field.
+     */
+    virtual model::GameField* getGameField() const;
 
-    bool isGameOver() const;
 
-    std::vector<model::FieldPosition> getLastGeneratedBalls();
-    std::vector<model::FieldPosition> getLastRemovedBalls();
+    /**
+     * Returns the current score.
+     *
+     * @return The score.
+     */
+    virtual int getTheScore() const;
+
+    /**
+     * Returns the applied amount of colors.
+     *
+     * @return The number of colors.
+     */
+    virtual int getAmountColors() const;
+
+    /**
+     * Returns the amount of new balls on each turn.
+     *
+     * @return The number of new balls.
+     */
+    virtual int getAmountNewBalls() const;
+
+    /**
+     * Returns the number of balls of the same color that are required to complete a line.
+     *
+     * @return The number of balls.
+     */
+    virtual int getAmountLine() const;
+
+    /**
+     * Checks whether the game is over.
+     *
+     * @return True if the game is over.
+     */
+    virtual bool isGameOver() const;
+
+    /**
+     * Returns the recently generated new balls.
+     *
+     * @return The last generated balls.
+     */
+    virtual std::vector<model::FieldPosition> getLastGeneratedBalls();
+
+    /**
+     * Returns the recently removed balls.
+     *
+     * @return The last removed balls.
+     */
+    virtual std::vector<model::FieldPosition> getLastRemovedBalls();
 
 
 private:
-    int amount_colors;
-    int amount_newballs;
-    int amount_line;
+    /**
+     * The number of different colors.
+     */
+    int amountColors;
 
+    /**
+     * The number of new balls on each turn.
+     */
+    int amountNewBalls;
+
+    /**
+     * The number of balls required for a line.
+     */
+    int amountLine;
+
+    /**
+     * The current number of turns.
+     */
     int rounds;
+
+    /**
+     * A pointer to the game field.
+     */
     model::GameField* theField;
 
+    /**
+     * The last generated balls.
+     */
     std::vector<model::FieldPosition> lastGeneratedBalls;
+
+    /**
+     * The last removed balls.
+     */
     std::vector<model::FieldPosition> lastRemovedBalls;
 
+    /**
+     * A pointer to the game controller.
+     */
     controller::GameActionController* actionController;
 
+    /**
+     * The game score.
+     */
     int theScore;
+
+    /**
+     * Flag for option set. True if the options are already set, false otherwise.
+     */
     bool optionsSet;
 
-    bool generateRandomBalls(int amount); // creates amount of balls at random positions, if this is not possible, the game ends
-    bool checkLines(model::FieldPosition pos); //checks for Lines to remove and returns true if any; counts scores
+    /**
+     * Creates random balls at random, yet possible field positions. If this is not possible, the game ends.
+     *
+     * @param amount The number of new balls to be created.
+     * @return True if the game is over.
+     */
+    bool generateRandomBalls(int amount);
+
+    /**
+     * Checks for Lines to be removed and returns true if there are any. Additionally, the score is increased.
+     *
+     * @param pos The field position to be checked. This should be the position of each newly generated ball and the destination of a moved ball.
+     * @return True if there are lined to be removed.
+     */
+    bool checkLines(model::FieldPosition pos);
 
 
 };
